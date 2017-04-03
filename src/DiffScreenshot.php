c7ca0e3e784ab1534f91a6a1ede2e7e8fa7a25b2
y-ohashi <y_ohashi@okwave.co.jp>
2017-04-03 19:45:29 +0900

    WIP

--- src/DiffScreenshot.php
+++ src/DiffScreenshot.php
@@ -12,7 +12,6 @@ class DiffScreenshot
         $img2 = new \Imagick();
         $img2->readImage($imgPath2);
         
-        /*
         $resizeWidth  = ($img1->getImageWidth() >= $img2->getImageWidth())   ? $img1->getImageWidth()  : $img2->getImageWidth();
         $resizeHeight = ($img1->getImageHeight() >= $img2->getImageHeight()) ? $img1->getImageHeight() : $img2->getImageHeight();
         
@@ -24,17 +23,14 @@ class DiffScreenshot
         
         $img1->extentImage((int)$resizeWidth, (int)$resizeHeight, (int)-($resizeWidth - $img1->getImageWidth()) / 2, 0);
         $img2->extentImage((int)$resizeWidth, (int)$resizeHeight, (int)-($resizeWidth - $img2->getImageWidth()) / 2, 0);
-        */
         
         $img1->resizeImage(1000, 1000, \Imagick::FILTER_LANCZOS, 1);
         $img2->resizeImage(1000, 1000, \Imagick::FILTER_LANCZOS, 1);
         
         $result = $img1->compareImages($img2, 1);
         
-        $fp = fopen($outputPath . $outputImgName . '.' . $outputImgType, 'wb');
-        $result[0]->setImageFormat($outputImgType);
-        fwrite($fp, $result[0]);
-        fclose($fp);
+        $imgName = $outputPath . $outputImgName . '.' . $outputImgType;
+        $result->writeImages($imgName, true);
         
         return $result[1];
     }
