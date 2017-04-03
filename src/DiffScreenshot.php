d3e6a64ceff88730ced7b603b54813fc96e0ef5b
y-ohashi <y_ohashi@okwave.co.jp>
2017-04-03 19:43:52 +0900

    WIP

--- src/DiffScreenshot.php
+++ src/DiffScreenshot.php
@@ -6,13 +6,13 @@ class DiffScreenshot
 {
     public static function compare($imgPath1, $imgPath2, $outputPath = './', $outputImgType = 'jpg', $outputImgName = 'diff')
     {
-        /*
         $img1 = new \Imagick();
         $img1->readImage($imgPath1);
         
         $img2 = new \Imagick();
         $img2->readImage($imgPath2);
         
+        /*
         $resizeWidth  = ($img1->getImageWidth() >= $img2->getImageWidth())   ? $img1->getImageWidth()  : $img2->getImageWidth();
         $resizeHeight = ($img1->getImageHeight() >= $img2->getImageHeight()) ? $img1->getImageHeight() : $img2->getImageHeight();
         
@@ -24,6 +24,7 @@ class DiffScreenshot
         
         $img1->extentImage((int)$resizeWidth, (int)$resizeHeight, (int)-($resizeWidth - $img1->getImageWidth()) / 2, 0);
         $img2->extentImage((int)$resizeWidth, (int)$resizeHeight, (int)-($resizeWidth - $img2->getImageWidth()) / 2, 0);
+        */
         
         $img1->resizeImage(1000, 1000, \Imagick::FILTER_LANCZOS, 1);
         $img2->resizeImage(1000, 1000, \Imagick::FILTER_LANCZOS, 1);
@@ -36,7 +37,6 @@ class DiffScreenshot
         fclose($fp);
         
         return $result[1];
-        */
     }
     
     public static function createAnimeGif($imgPath1, $imgPath2, $outputPath = './', $outputImgName = 'anime-diff')
