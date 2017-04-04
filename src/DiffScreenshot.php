bc009110f1203c434d8b763735a0508560c5f45a
y-ohashi <y_ohashi@okwave.co.jp>
2017-04-04 18:43:57 +0900

    WIP

--- src/DiffScreenshot.php
+++ src/DiffScreenshot.php
@@ -26,7 +26,7 @@ class DiffScreenshot
             $img2->extentImage($resizeWidth, $resizeHeight, (int)-($resizeWidth - $img2->getImageWidth()) / 2, 0);
         } catch (Exception $e) {
             echo "test1";
-            throw $e;
+            return 0;
         }
         
         try {
@@ -34,49 +34,57 @@ class DiffScreenshot
             $img2->resizeImage(1000, 1000, \Imagick::FILTER_LANCZOS, 1);
         } catch (Exception $e) {
             echo "test2";
-            throw $e;
+            return 0;
         }
         
         try {
             $result = $img1->compareImages($img2, 1);
         } catch (Exception $e) {
             echo "test3";
-            throw $e;
+            return 0;
         }
         
         try {
             $fp = fopen($outputPath . $outputImgName . '.' . $outputImgType, 'wb');
         } catch (Exception $e) {
             echo "test4";
-            throw $e;
+            return 0;
         }
         
         try {
             $result[0]->setImageFormat($outputImgType);
         } catch (Exception $e) {
             echo "test5";
-            throw $e;
+            return 0;
         }
         
         try {
             fwrite($fp, $result[0]);
         } catch (Exception $e) {
             echo "test6";
-            throw $e;
+            return 0;
         }
         
         try {
             fclose($fp);
         } catch (Exception $e) {
             echo "test7";
-            throw $e;
+            return 0;
+        }
+        
+        try {
+            $img1->clear();
+            $img2->clear();
+        } catch (Exception $e) {
+            echo "test7-2";
+            return 0;
         }
         
         try {
             return (int)$result[1];
         } catch (Exception $e) {
             echo "test8";
-            throw $e;
+            return 0;
         }
     }
     
