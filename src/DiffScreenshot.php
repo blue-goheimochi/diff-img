4a539686bd406bb7b02e492be6ca6d3ea86ab653
y-ohashi <y_ohashi@okwave.co.jp>
2017-04-05 10:09:09 +0900

    WIP

--- src/DiffScreenshot.php
+++ src/DiffScreenshot.php
@@ -6,88 +6,89 @@ class DiffScreenshot
 {
     public static function compare($imgPath1, $imgPath2, $outputPath = './', $outputImgType = 'jpg', $outputImgName = 'diff')
     {
-        // $img1 = new \Imagick();
-        // $img1->readImage($imgPath1);
-        // $img1->scaleImage(1000, 0);
-        // 
-        // $img2 = new \Imagick();
-        // $img2->readImage($imgPath2);
-        // $img2->scaleImage(1000, 0);
-        // 
-        // $resizeWidth  = ($img1->getImageWidth() >= $img2->getImageWidth())   ? $img1->getImageWidth()  : $img2->getImageWidth();
-        // $resizeHeight = ($img1->getImageHeight() >= $img2->getImageHeight()) ? $img1->getImageHeight() : $img2->getImageHeight();
-        // 
-        // if ($resizeWidth > $resizeHeight) {
-        //     $resizeHeight = $resizeWidth;
-        // } else {
-        //     $resizeWidth = $resizeHeight;
-        // }
-        // 
-        // try {
-        //     // $img1->extentImage($resizeWidth, $resizeHeight, (int)-($resizeWidth - $img1->getImageWidth()) / 2, 0);
-        //     // $img2->extentImage($resizeWidth, $resizeHeight, (int)-($resizeWidth - $img2->getImageWidth()) / 2, 0);
-        // } catch (Exception $e) {
-        //     echo "test1";
-        //     return 0;
-        // }
-        // 
-        // try {
-        //     // $img1->resizeImage(1000, 1000, \Imagick::FILTER_LANCZOS, 1);
-        //     // $img2->resizeImage(1000, 1000, \Imagick::FILTER_LANCZOS, 1);
-        // } catch (Exception $e) {
-        //     echo "test2";
-        //     return 0;
-        // }
-        // 
-        // try {
-        //     $result = $img1->compareImages($img2, 1);
-        // } catch (Exception $e) {
-        //     echo "test3";
-        //     return 0;
-        // }
-        // 
-        // try {
-        //     $fp = fopen($outputPath . $outputImgName . '.' . $outputImgType, 'wb');
-        // } catch (Exception $e) {
-        //     echo "test4";
-        //     return 0;
-        // }
-        // 
-        // try {
-        //     $result[0]->setImageFormat($outputImgType);
-        // } catch (Exception $e) {
-        //     echo "test5";
-        //     return 0;
-        // }
-        // 
-        // try {
-        //     fwrite($fp, $result[0]);
-        // } catch (Exception $e) {
-        //     echo "test6";
-        //     return 0;
-        // }
-        // 
-        // try {
-        //     fclose($fp);
-        // } catch (Exception $e) {
-        //     echo "test7";
-        //     return 0;
-        // }
-        // 
-        // try {
-        //     $img1->clear();
-        //     $img2->clear();
-        // } catch (Exception $e) {
-        //     echo "test7-2";
-        //     return 0;
-        // }
-        // 
-        // try {
-        //     return (int)$result[1];
-        // } catch (Exception $e) {
-        //     echo "test8";
-        //     return 0;
-        // }
+        $img1 = new \Imagick();
+        $img1->readImage($imgPath1);
+        $img1->scaleImage(1000, 0);
+        
+        $img2 = new \Imagick();
+        $img2->readImage($imgPath2);
+        $img2->scaleImage(1000, 0);
+        
+        $resizeWidth  = ($img1->getImageWidth() >= $img2->getImageWidth())   ? $img1->getImageWidth()  : $img2->getImageWidth();
+        $resizeHeight = ($img1->getImageHeight() >= $img2->getImageHeight()) ? $img1->getImageHeight() : $img2->getImageHeight();
+        
+        if ($resizeWidth > $resizeHeight) {
+            $resizeHeight = $resizeWidth;
+        } else {
+            $resizeWidth = $resizeHeight;
+        }
+        
+        try {
+            // $img1->extentImage($resizeWidth, $resizeHeight, (int)-($resizeWidth - $img1->getImageWidth()) / 2, 0);
+            // $img2->extentImage($resizeWidth, $resizeHeight, (int)-($resizeWidth - $img2->getImageWidth()) / 2, 0);
+        } catch (Exception $e) {
+            echo "test1";
+            return 0;
+        }
+        
+        try {
+            // $img1->resizeImage(1000, 1000, \Imagick::FILTER_LANCZOS, 1);
+            // $img2->resizeImage(1000, 1000, \Imagick::FILTER_LANCZOS, 1);
+        } catch (Exception $e) {
+            echo "test2";
+            return 0;
+        }
+        /*
+        try {
+            $result = $img1->compareImages($img2, 1);
+        } catch (Exception $e) {
+            echo "test3";
+            return 0;
+        }
+        
+        try {
+            $fp = fopen($outputPath . $outputImgName . '.' . $outputImgType, 'wb');
+        } catch (Exception $e) {
+            echo "test4";
+            return 0;
+        }
+        
+        try {
+            $result[0]->setImageFormat($outputImgType);
+        } catch (Exception $e) {
+            echo "test5";
+            return 0;
+        }
+        
+        try {
+            fwrite($fp, $result[0]);
+        } catch (Exception $e) {
+            echo "test6";
+            return 0;
+        }
+        
+        try {
+            fclose($fp);
+        } catch (Exception $e) {
+            echo "test7";
+            return 0;
+        }
+        
+        try {
+            $img1->clear();
+            $img2->clear();
+        } catch (Exception $e) {
+            echo "test7-2";
+            return 0;
+        }
+        
+        try {
+            return (int)$result[1];
+        } catch (Exception $e) {
+            echo "test8";
+            return 0;
+        }
+        */
         return 0;
     }
     
